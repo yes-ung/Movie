5137bfc1a7c6f162749d817248b27cf34e8ec8eb
yes-ung <yete0401@naver.com>
2025-05-08 16:58:15 +0900

    Add files via upload

---
+++ js/nav.js
@@ -0,0 +1,47 @@
+$(function(){
+    $(".hover1").hover(function(){
+        $(".p-hover1").show();
+    } , function(){
+        $(".p-hover1").hide();
+    });
+ });
+$(function(){
+    $(".hover2").hover(function(){
+        $(".p-hover2").show();
+    } , function(){
+        $(".p-hover2").hide();
+    });
+ });
+$(function(){
+    $(".hover3").hover(function(){
+        $(".p-hover3").show();
+    } , function(){
+        $(".p-hover3").hide();
+    });
+ });
+$(function(){
+    $(".hover4").hover(function(){
+        $(".p-hover4").show();
+    } , function(){
+        $(".p-hover4").hide();
+    });
+ });
+$(function(){
+    $(".hover5").hover(function(){
+        $(".p-hover5").show();
+    } , function(){
+        $(".p-hover5").hide();
+    });
+ });
+
+ let swiper = new Swiper(".mySwiper",{
+    autoplay:{
+        delay:300000
+    },
+
+pagination:{
+    el:".swiper-pagination",
+    dynamicBullets: true,
+},
+
+})

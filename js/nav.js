eb9f1ecadb39d5e761e250d7839d37e7e748f7bd
yes-ung <yete0401@naver.com>
2025-05-09 10:43:13 +0900

    Update nav.js

--- js/nav.js
+++ js/nav.js
@@ -35,10 +35,7 @@ $(function(){
  });
 
  let swiper = new Swiper(".mySwiper",{
-    autoplay:{
-        delay:300000
-    },
-
+    
 pagination:{
     el:".swiper-pagination",
     dynamicBullets: true,

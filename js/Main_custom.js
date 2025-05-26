eb5439d3b2e770e82c8371ef024ca26ed8b9b4a5
yes-ung <yete0401@naver.com>
2025-05-26 12:06:02 +0900

    Delete js/Main_custom.js

--- js/Main_custom.js
+++
@@ -1,9 +0,0 @@
-const mainSwiper = new Swiper(".swiper", {
-  pagination: {
-    el: ".swiper-pagination",
-  },
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
-});

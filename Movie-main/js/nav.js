bf76d50e84d778310e5283f200c0df261799fe99
yes-ung <yete0401@naver.com>
2025-05-26 12:22:12 +0900

    Add files via upload

---
+++ Movie-main/js/nav.js
@@ -0,0 +1,38 @@
+$(function(){
+    $(".hover1").hover(function(){
+        $(".p-hover1").show();
+    } , function(){
+        $(".p-hover1").hide();
+    });
+ });
+$(function(){
+    $(".hover2").hover(function(){
+        $(".p-hover2").show();
+    } , function(){
+        $(".p-hover2").hide();
+    });
+ });
+$(function(){
+    $(".hover3").hover(function(){
+        $(".p-hover3").show();
+    } , function(){
+        $(".p-hover3").hide();
+    });
+ });
+$(function(){
+    $(".hover4").hover(function(){
+        $(".p-hover4").show();
+    } , function(){
+        $(".p-hover4").hide();
+    });
+ });
+$(function(){
+    $(".hover5").hover(function(){
+        $(".p-hover5").show();
+    } , function(){
+        $(".p-hover5").hide();
+    });
+ });
+
+ let navSwiper = new Swiper(".mySwiper2",{
+    })
